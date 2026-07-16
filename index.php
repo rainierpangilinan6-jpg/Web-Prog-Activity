303e869611f19aec6519c9c9ba4d3c8d7d770ffc
radray <rainierpangilinan6@gmail.com>
2026-07-16 16:11:53 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "My name is: Rainier Pangilinan <br>";
+echo "I am 22 yrs old <br>";
+echo "My favorite subject is: Mathematics <br>";
+echo "My dream job is: Computer Engineer <br>";
+echo "My motto in life: Time is gold <br>";
+echo "Here is my ASCII ART: <br>";
+echo "^   ^ <br>";
+echo "(o o) <br>";
+echo " \ /  <br>";
+echo "  *   <br>";
+
+?>
