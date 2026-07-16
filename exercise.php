fa907c21edb312ef5917c6ce3dd6c74a24cc6eba
radray <rainierpangilinan6@gmail.com>
2026-07-16 16:27:07 +0800

    Add files via upload

---
+++ exercise.php
@@ -0,0 +1,18 @@
+<?php
+$age = 99;
+
+
+if ($age >= 60){
+    echo "Senior Citizen";
+}
+elseif($age >= 18){
+    echo "Adult";
+}
+elseif($age >= 13){
+    echo "Teenager";
+}
+else {
+    echo "Child";
+}
+
+?>
